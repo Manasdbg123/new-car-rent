b47f73a94de01d77b9bb363d7b643e5b28746e62
Manasdbg123 <kaustuk2003@gmail.com>
2026-06-02 18:34:17 +0530

    Stop tracking log files to fix lock issues

--- src/main/java/com/carrental/CarRentalApplication.java
+++ src/main/java/com/carrental/CarRentalApplication.java
@@ -1,4 +1,16 @@
 package com.carrental;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+import static org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO;
+
+@SpringBootApplication
+@EnableCaching
+@EnableSpringDataWebSupport(pageSerializationMode = VIA_DTO)
 public class CarRentalApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CarRentalApplication.class, args);
+    }
 }
